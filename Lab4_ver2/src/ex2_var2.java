6e98fed5e39c8f74076d6dd079f1ce8ed1cf1aea
m4sk3k <maksmakarow01@mail.ru>
2020-06-06 13:13:12 +0900

    Lab4_ver2

---
+++ Lab4_ver2/src/ex2_var2.java
@@ -0,0 +1,34 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ex2_var2 {
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        try {
+            System.out.println("Введите кол-во строк: ");
+            int x = sc.nextInt();
+            System.out.println("Введите кол-во столбцов: ");
+            int y = sc.nextInt();
+            int[][] arr = new int[x][y];
+            for (int i = 0; i < x; i++) {
+                for (int j = 0; j < y; j++) {
+                    arr[i][j] = (int) (Math.random() * 10);
+                    System.out.print("[" + arr[i][j] + "] ");
+                }
+                System.out.println();
+            }
+            System.out.println("Какой столбец вывести?");
+            int k = sc.nextInt();
+            for (int i = 0; i < arr.length; i++) {
+                System.out.println("[" + arr[i][k - 1] + "]");
+            }
+
+        } catch (InputMismatchException e) {
+            System.out.println("Вы ввели не целое число: " + e);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Такого столбца нет: " + e);
+        }
+    }
+}
+
